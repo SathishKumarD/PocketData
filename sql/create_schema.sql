7b4e7ce5b6387a7af378e9422e79a1d331e5af38
Sathish Kumar Deivasigamani <sdeivasi@buffalo.edu>
2015-09-17 00:55:42 -0400

    changed the order of columns and added insert user data

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -5,34 +5,34 @@ use pocket_data;
 
 -- Create User Table
 CREATE TABLE IF NOT EXISTS user(
-user_id INT NOT NULL AUTO_INCREMENT,
 unique_id 			VARCHAR(40), 		-- guid present in the log file
 username		VARCHAR(20),			-- username for easy remembrance
+user_id INT NOT NULL AUTO_INCREMENT,
 PRIMARY KEY (user_id)
 );
 
 -- Create Application table
 CREATE TABLE IF NOT EXISTS app(
-app_id 			INT NOT NULL AUTO_INCREMENT, 			
 appname			VARCHAR(20),			-- name of the android application
+app_id 			INT NOT NULL AUTO_INCREMENT, 	
 PRIMARY KEY	 (app_id)
 );
 
 -- Create table UserApps table
 CREATE TABLE IF NOT EXISTS user_app(
-user_app_id 	INT NOT NULL AUTO_INCREMENT, 			
 user_id			INT,
 app_id 			INT,						
+user_app_id 	INT NOT NULL AUTO_INCREMENT,
 PRIMARY KEY	 (user_app_id)
 );
 
 -- Create table UserApps table
 CREATE TABLE IF NOT EXISTS user_app_process(
-user_app_process_id 	INT NOT NULL AUTO_INCREMENT, 			
 user_id			INT,
 app_id 			INT,	
 process_id 		INT,
 thread_id		INT,					
+user_app_process_id 	INT NOT NULL AUTO_INCREMENT, 
 PRIMARY KEY	 (user_app_process_id)
 );
 
@@ -40,7 +40,6 @@ PRIMARY KEY	 (user_app_process_id)
 
 -- Create table sql_log table
 CREATE TABLE IF NOT EXISTS sql_log(
-log_id 	INT NOT NULL AUTO_INCREMENT, 			
 unique_id		INT,
 ticks 			INT,	
 ticks_ms 		DOUBLE,
@@ -48,5 +47,6 @@ date_time		DATETIME,
 process_id		INT,
 thread_id		INT,	
 raw_data		VARCHAR(400),		
+log_id 	INT NOT NULL AUTO_INCREMENT, 
 PRIMARY KEY	 (log_id)
 ) 
