92e04deced96da7311d878c55dd3cc04639346e4
Sankar D <san.gct@gmail.com>
2015-09-26 04:11:57 +0000

    Removed a table, added one, updated naming guidelines for this SQL

--- sql/create_schema.sql
+++ sql/create_schema.sql
@@ -1,74 +1,91 @@
--- Create Schema
+-- $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+-- $$$$$$$$$$$$$$$    NAMING STYLE 		$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+-- $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+--
+-- 1) All column/table names are lower cased
+-- 2) Primary Key is named <table_name>_id
+-- 3) Create tables have PK as the first column
+-- 4) Relationship tables are named <table1_name>_<table2_name>
+-- 5) FK names exactly match PK name
+-- 6) Each word in the Column/Table are seperated by '_'
+-- 7) PK if present in a table should be first column in CREATE STATEMENT
+-- 
+-- $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+-- $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
 CREATE SCHEMA IF NOT EXISTS pocket_data;
 
 use pocket_data;
 
--- Create User Table
 CREATE TABLE IF NOT EXISTS user(
-unique_id 			VARCHAR(40), 		-- guid present in the log file
-username		VARCHAR(20),			-- username for easy remembrance
-user_id INT NOT NULL AUTO_INCREMENT,
+user_id 			INT NOT NULL,
+guid	 			VARCHAR(40), 	-- guid present in the log file
+user_name			VARCHAR(20),	-- username for easy remembrance
 PRIMARY KEY (user_id)
 );
 
--- Create Application table
 CREATE TABLE IF NOT EXISTS app(
-appname			VARCHAR(20),			-- name of the android application
-app_id 			INT NOT NULL AUTO_INCREMENT, 	
+app_id 				INT NOT NULL, 	
+app_name			VARCHAR(20),	-- name of the android application
 PRIMARY KEY	 (app_id)
 );
 
--- Create table UserApps table
-CREATE TABLE IF NOT EXISTS user_app(
-user_id			INT,
-app_id 			INT,						
-user_app_id 	INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY	 (user_app_id)
-);
-
--- Create table UserApps table
+-- User_App table is redundant as this table captures it all.
+-- Not a heavy table, DISTINCT user_id, app_id ==> user_app Table
 CREATE TABLE IF NOT EXISTS user_app_process(
-user_id			INT,
-app_id 			INT,	
-process_id 		INT,
-thread_id		INT,					
-user_app_process_id 	INT NOT NULL AUTO_INCREMENT, 
+user_app_process_id 	INT NOT NULL, 
+user_id					INT,
+app_id 					INT,	
+process_id 				INT,
+thread_id				INT,	
+unix_ticks_ms			DOUBLE, 	-- ORDER BY unix_ticks_ms DESC ==> app's latest Proc, thread id				
 PRIMARY KEY	 (user_app_process_id)
 );
 
-
-
--- Create table sql_log table
 CREATE TABLE IF NOT EXISTS sql_log(
-ticks 			INT,	
-ticks_ms 		DOUBLE,
-date_time		DATETIME,	
-process_id		INT,
-thread_id		INT,	
-raw_data		VARCHAR(400),		
-log_id 	INT NOT NULL AUTO_INCREMENT, 
+log_id 				INT NOT NULL, 	-- log_id is the same for sql_log and Analytics
+ticks 				INT,	
+ticks_ms 			DOUBLE,
+date_time			DATETIME,
+user_app_process_id	INT,			-- FK to user_app_process table
+process_id			INT,
+thread_id			INT,	
+raw_data			TEXT,	
 PRIMARY KEY	 (log_id)
 );
 
--- Create table sql_log table
 CREATE TABLE IF NOT EXISTS analytics(
-
-ticks 			INT,	
-ticks_ms 		DOUBLE,
-date_time		DATETIME,	
-user_id 		INT,
-app_name		INT,	
-query_type 			VARCHAR(10), -- SELECT, INSERT, UPDATE ETC.,
+log_id				INT NOT NULL,  	-- log_id is the same for sql_log and Analytics
+ticks 				INT,	
+ticks_ms 			DOUBLE,
+date_time			DATETIME,	
+user_id 			INT,			-- FK to user table
+app_id				INT,			-- FK to app table
+query_type 			VARCHAR(10), 	-- SELECT, INSERT, UPDATE ETC.,
 outerjoin_count		INT,
 distinct_count		INT,
 limit_count			INT,
 orderby_count		INT,
-aggregate_count	INT,
-groupby_count	INT,
-union_count		INT,
-join_width		INT,
-where_count 	INT,
-log_id			INT NOT NULL AUTO_INCREMENT,
-
+aggregate_count		INT,
+groupby_count		INT,
+union_count			INT,
+join_width			INT,
+where_count 		INT,
 PRIMARY KEY	 (log_id)
-); 
+);
+
+CREATE TABLE IF NOT EXISTS unparsed_log_lines(
+unparsed_log_lines_id 	INT NOT NULL, 
+file_location 			VARCHAR(200),	
+file_line_number 		INT,
+raw_data				TEXT,
+exception_trace 		VARCHAR(400),	
+PRIMARY KEY	 (unparsed_log_lines_id)
+);
+
+CREATE TABLE IF NOT EXISTS primary_key_sequence(
+table_name				VARCHAR(70),
+primary_key_col_name	VARCHAR(70),
+next_id_value			INT,
+PRIMARY KEY	 (table_name, primary_key_col_name)
+);
